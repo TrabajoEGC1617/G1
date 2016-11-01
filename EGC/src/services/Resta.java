bd06bed8e520653ed8ca0b1a8832a94cd89e7cca
Rafa Garcia <Rafa Garcia>
2016-11-01 17:55:12 +0100

    funcionalidad resta subida a rama1

---
+++ EGC/src/services/Resta.java
@@ -0,0 +1,12 @@
+package services;
+
+public class Resta {
+
+	public static Integer restaEntera(int a, int b){
+		return a-b;
+	}
+	
+	public static Double restaReal(double a, double b){
+		return a-b;
+	}
+}
---
+++ EGC/src/test/RestaTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import services.Resta;
+
+public class RestaTest {
+
+	@Test
+	public void restaEnteraTest(){
+		System.out.println(Resta.restaEntera(3, 7));
+	}
+	
+	@Test
+	public void restaRealTest(){
+		System.out.println(Resta.restaReal(10.2, 2.0));
+	}
+}
